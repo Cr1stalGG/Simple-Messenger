4ac26750a8fe1ad15ce34947cbe02accdbc90955
Alexander Savitskiy <alexandersavickij2@gmail.com>
2023-06-10 16:39:38 +0300

    Add DTO to auth

---
+++ src/main/java/com/simplemessenger/entity/dto/security/RegistrationRequest.java
@@ -0,0 +1,13 @@
+package com.simplemessenger.entity.dto.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegistrationRequest {
+    private String email;
+    private String password;
+}
