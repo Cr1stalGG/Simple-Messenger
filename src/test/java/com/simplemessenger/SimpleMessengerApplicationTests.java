f89ae9095b9647892843bcf8c2da8c514702b145
Alexander Savitskiy <alexandersavickij2@gmail.com>
2023-06-10 13:31:45 +0300

    Initial commit

---
+++ src/test/java/com/simplemessenger/SimpleMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplemessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleMessengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
